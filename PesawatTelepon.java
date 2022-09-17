3cfb6b4a2dbec384069a69432ed86dbeaad21012
Dea05 <dearisa65@gmail.com>
2022-09-17 22:38:26 +0700

    Create PesawatTelepon.java

---
+++ PesawatTelepon.java
@@ -0,0 +1,12 @@
+
+  public class PesawatTelepon{
+	String bentuk, kabel, gagang_telepon;
+
+	String terima_panggilan(String t){
+		return t;
+	}
+
+	String melakukan_panggilan(String m){
+		return m;
+	}
+}
